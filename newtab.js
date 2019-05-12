f4f9acb80b5a3e9fe79bc18a7a59b09211d78b58
Penciler <eraser41@livemail.tw>
2019-05-13 07:15:04 +0800

    add function to choose breed in drop down list

--- newtab.js
+++ newtab.js
@@ -1,13 +1,24 @@
-var img = document.createElement("img");
+function getPic(){
+	var img = document.createElement("img");
 
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-    img.src = myJson.message;
-    var src = document.getElementById("x");
- 	src.appendChild(img);
-  });
+	var breed = document.getElementById("breed").value;
+	//console.log('breed:' + breed);
+	var url = "https://dog.ceo/api/breed/" + breed + "/images/random";
 
+	//fetch("https://dog.ceo/api/breeds/image/random")
+	fetch(url)
+	  .then(function(response) {
+	    return response.json();
+	  })
+	  .then(function(myJson) {
+	    console.log(JSON.stringify(myJson));
+	    img.src = myJson.message;
+	    var src = document.getElementById("x");
+	 	src.appendChild(img);
+	 	//document.getElementById("x") = img;
+	  });
+}
+
+document.getElementById("breed").addEventListener("change", getPic);
+
+document.getElementById("breed").onchange = getPic();
